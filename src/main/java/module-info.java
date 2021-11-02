75c66c5ffc94882748a7350a36e282bf8d0fc3d3
HaoNguyen <hao.q.nguyen.2000@gmail.com>
2021-11-01 23:44:23 -0400

    Add launch

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module ucf.assignemtns {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens ucf.assignments to javafx.fxml;
+    exports ucf.assignments;
+}
--- src/main/java/ucf/assignments/App.java
+++ src/main/java/ucf/assignments/App.java
@@ -8,12 +8,26 @@
 
 package ucf.assignments;
 
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("App.fxml"));
+        //Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        //stage.setScene(scene);
+        stage.show();
+    }
 
     public static void main(String[] args) {
-
-        System.out.println("hello world");
-
+        launch();
     }
 
 }
